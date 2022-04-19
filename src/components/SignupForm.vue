dec94c1144cd8c67a9a839e56b882614751465e4
gnoul_ <ttluong4869@gmail.com>
2022-04-19 20:45:41 +0700

    Create SignupForm and LoginForm

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="text" required placeholder="username" v-model="username" />
+        <input type="email" required placeholder="email" v-model="email" />
+        <input type="password" required placeholder="password" v-model="password" />
+        <button>Signup</button>
+    </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+    setup() {
+        const username = ref('');
+        const email = ref('');
+        const password = ref('');
+
+        const handleSubmit = () => {
+            console.log(username.value);
+            console.log(email.value);
+            console.log(password.value);
+        };
+
+        return { username, email, password, handleSubmit };
+    },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="home container">
         <p>Home</p>
+        <SignupForm />
+        <LoginForm />
     </div>
 </template>
 
 <script>
+import SignupForm from '@/components/SignupForm.vue';
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
     name: 'HomeView',
+    components: { SignupForm, LoginForm },
 };
 </script>
 
@@ -14,5 +20,23 @@ export default {
 .home {
     text-align: center;
     padding: 20px 0;
+}
+
+.home form {
+    width: 300px;
+    margin: 20px auto;
+}
+.home label {
+    display: block;
+    margin: 20px 0 10px;
+}
+.home input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
 }
 </style>
