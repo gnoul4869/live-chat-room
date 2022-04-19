91f791eabfe792deb02286db9ea567f001b7ef6f
gnoul_ <ttluong4869@gmail.com>
2022-04-20 00:15:13 +0700

    Create toggle functionality for signup and login forms

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
     <div class="home container">
         <p>Home</p>
-        <SignupForm />
-        <LoginForm />
+        <div v-if="showLogin">
+            <LoginForm />
+            <p>No account yet? <span @click="showLogin = false">Signup</span> instead</p>
+        </div>
+        <div v-else>
+            <SignupForm />
+            <p>Already registered? <span @click="showLogin = true">Login</span> instead</p>
+        </div>
     </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 import SignupForm from '@/components/SignupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 export default {
     name: 'HomeView',
     components: { SignupForm, LoginForm },
+    setup() {
+        const showLogin = ref(true);
+
+        return { showLogin };
+    },
 };
 </script>
 
@@ -38,5 +50,16 @@ export default {
     outline: none;
     color: #999;
     margin: 10px auto;
+}
+.home span {
+    font-weight: bold;
+    cursor: pointer;
+    transition: color 0.1s ease-in;
+}
+.home span:hover {
+    color: #5ae4ca;
+}
+.home button {
+    margin: 20px auto;
 }
 </style>
