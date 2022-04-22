8e0dd5c82aba21eac6dc4e19390f941435bc1cd8
gnoul_ <ttluong4869@gmail.com>
2022-04-22 23:17:51 +0700

    Watch user state to protect chatroom from unathenticated user

--- src/views/ChatroomView.vue
+++ src/views/ChatroomView.vue
@@ -5,10 +5,23 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+import { watch } from 'vue';
 import Navbar from '@/components/Navbar.vue';
+import getUser from '@/composables/getUser';
 
 export default {
     components: { Navbar },
+    setup() {
+        const router = useRouter();
+        const { user } = getUser();
+
+        watch(user, () => {
+            if (!user.value) {
+                router.push({ name: 'Home' });
+            }
+        });
+    },
 };
 </script>
 
