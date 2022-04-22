641145294ec7e71a8ba96bd34248f7ce7a06abfd
gnoul_ <ttluong4869@gmail.com>
2022-04-22 22:54:46 +0700

    Make app wait for fireAuth before initializing

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { onAuthStateChanged } from '@firebase/auth';
+import { fireAuth } from '@/firebase/config';
+
 import '@/assets/main.css';
 
-createApp(App).use(router).mount('#app');
+let app;
+
+onAuthStateChanged(fireAuth, () => {
+    if (!app) {
+        app = createApp(App).use(router).mount('#app');
+    }
+});
